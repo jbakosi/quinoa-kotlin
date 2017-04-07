29b39a97bf7f87a8bc0c2671e6c1b15a9d6a3d74
Jozsef Bakosi <jbakosi@lanl.gov>
2017-04-07 08:45:30 -0600

    Test

--- .teamcity/QuinoaKotlin_Mac/buildTypes/QuinoaKotlin_Mac_Matrix.kt
+++ .teamcity/QuinoaKotlin_Mac/buildTypes/QuinoaKotlin_Mac_Matrix.kt
@@ -50,10 +50,10 @@ object QuinoaKotlin_Mac_Matrix : Template({
     triggers {
         vcs {
             id = "vcsTrigger"
-            triggerRules = """
-                -:comment=[ci skip]:**
-                -:comment=[skip ci]:**
-            """.trimIndent()
+//             triggerRules = """
+//                 -:comment=[ci skip]:**
+//                 -:comment=[skip ci]:**
+//             """.trimIndent()
             perCheckinTriggering = true
             groupCheckinsByCommitter = true
         }
